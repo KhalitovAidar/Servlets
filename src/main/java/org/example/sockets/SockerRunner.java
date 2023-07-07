1781c6908f04baf40e22223a0b874a6f7cf36b1f
KhalitovAidar <khalitovaidar2404@gmail.com>
2023-07-07 11:32:23 +0300

    Add simple sockets practise

---
+++ src/main/java/org/example/sockets/SockerRunner.java
@@ -0,0 +1,33 @@
+package org.example.sockets;
+
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class SockerRunner {
+    public static void main(String[] args) throws UnknownHostException {
+        // http 80
+        // https 443
+        // tcp
+        InetAddress inetAddress = Inet4Address.getByName("localhost");
+
+        try(
+                Socket socket = new Socket(inetAddress, 7777);
+                DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+                DataInputStream is = new DataInputStream(socket.getInputStream());
+                Scanner scanner = new Scanner(System.in);
+                ) {
+            while (scanner.hasNextLine()) {
+                String request = scanner.nextLine();
+                os.writeUTF(request);
+                System.out.println("Response from server: "  + is.readUTF());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/org/example/sockets/SocketServerRunner.java
@@ -0,0 +1,29 @@
+package org.example.sockets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketServerRunner {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(7777);
+             Socket socket = serverSocket.accept();
+             DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+             DataInputStream is = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in);
+        ) {
+            String request = is.readUTF();
+            while (!"stop".equals(request)) {
+                System.out.println("Client request: " + request);
+                String response = scanner.nextLine();
+                os.writeUTF(response);
+                request = is.readUTF();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
